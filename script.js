5e20b53ffc95764559d912aa59fc21be3bcae1d3
wijman <wish6242@gmail.com>
2023-01-15 03:09:11 +0530

    added more functionality

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 let id1 = 0;
-let msg = document.getElementById("msg");
 let arr = [];
+let msg = document.getElementById("msg");
 let trData = document.getElementById("trData");
+let empNo = document.getElementById("empNo").classList;
 let fun1 = () => {
   let inputs = document.getElementsByTagName("input");
   let naam = inputs[0].value;
@@ -15,8 +16,7 @@ let fun1 = () => {
     id1++;
     msg.innerHTML =
       '<span style="color:green"> Success: Employee Added! </span>';
-    document.getElementById("empNo").innerHTML = "";
-    // let buttons=document.getElementById('buttons');
+    empNo.add("hidden");
     let obj = {
       id: id1,
       name: naam,
@@ -26,56 +26,38 @@ let fun1 = () => {
     arr.push(obj);
 
     fun4();
-    // let trData = document.getElementById("trData");
-    // trData.innerHTML = "";
-    // for (let element of arr) {
-    //   trData.innerHTML += `
-                
-    //             <tr>
 
-    //             <td> ${element.id} </td> 
-    //             <td>Name:${element.name}</td>
-    //             <td>Profession: ${element.profession}</td> 
-    //             <td>Age: ${element.age}</td>
-    //             <td><button style="border-radius: 30px;" onclick="fun3(id-1)">Delete User</button>
-    //             </td>
-
-    //             </tr>
-                
-    //             `;
-    // }
-
-    // inputs[0].value = "";
-    // inputs[1].value = "";
-    // inputs[2].value = "";
+    inputs[0].value = "";
+    inputs[1].value = "";
+    inputs[2].value = "";
   }
+  msg.classList.remove("hidden");
 };
 let fun2 = () => {
   msg.innerHTML = "";
+  msg.classList.remove("hidden");
 };
-let fun3=(i)=>{
-    arr.splice(i,1);
-    fun4();
-}
-let fun4=()=>{
-    
-    trData.innerHTML = "";
-    let a=0;
-    for (let element of arr) {
-      trData.innerHTML += `
-                <tbody>
-                <tr>
-
-                <td> ${element.id} </td> 
-                <td>Name:${element.name}</td>
-                <td>Profession: ${element.profession}</td> 
-                <td>Age: ${element.age}</td>
-                <td><button style="border-radius: 30px;" onclick="fun3(${a})">Delete User</button>
-                </td>
-
-                </tr>
-                </tbody>
-                `;
-                a++;
-    }
-}
+let fun3 = (i) => {
+  arr.splice(i, 1);
+  fun4();
+  if (arr.length == 0) {
+    empNo.remove("hidden");
+    msg.classList.add("hidden");
+  }
+};
+let fun4 = () => {
+  trData.innerHTML = "";
+  let a = 0;
+  for (let element of arr) {
+    trData.innerHTML += `
+            <tr>
+            <td> ${element.id}. </td> 
+            <td>Name:${element.name}</td>
+            <td>Profession: ${element.profession}</td> 
+            <td>Age: ${element.age}</td>
+            <td><button onclick="fun3(${a})">Delete User</button></td>
+            </tr>
+            `;
+    a++;
+  }
+};
